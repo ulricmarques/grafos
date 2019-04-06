adde5f01d8861b85c156fd6ded610bd0d0c03e63
Ulric Marques <ulricmf@gmail.com>
2019-04-06 16:01:19 -0300

    Update ep1.c

--- ep1.c
+++ ep1.c
@@ -5,8 +5,8 @@
  *
  *   EQUIPE:
  *       - Cláudia Mariana Costa Maia
- *       - 
- 		 -
+ *       - Jorge Girão Limaverde Lima
+ *		 - Thalia Nascimento de Sousa Freitas
  *       - Ulric Marques Ferreira
  */
 
@@ -19,21 +19,21 @@
 #include <math.h>
 #include <limits.h>
 
-/* Vértices de grafos são representados por objetos do tipo vertex. */ 
-#define vertex int
+/* Vértices de grafos são representados por objetos do tipo vertice. */ 
+#define vertice int
 #define INF INT_MAX
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A estrutura graph representa um grafo. 
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A estrutura grafo representa um grafo. 
 	O campo adj é um ponteiro para a matriz de adjacências do grafo. 
 	O campo V contém o número de vértices e o campo A contém o número de arcos do grafo. */
-	struct graph {
+	struct grafo {
 	   int V; 
 	   int A; 
 	   int **adj; 
 	};
 	
-	/* Um Graph é um ponteiro para um graph, ou seja, um Graph contém o endereço de um graph. */
-	typedef struct graph *Graph;
+	/* Um Grafo é um ponteiro para um grafo, ou seja, um Grafo contém o endereço de um grafo. */
+	typedef struct grafo *Grafo;
 
 void limpaTela(){
     #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
@@ -45,10 +45,10 @@ void limpaTela(){
     #endif
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função MATRIXint() aloca uma matriz com linhas 1..r e colunas 1..c. 
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função alocaMatriz() aloca uma matriz com linhas 1..r e colunas 1..c. 
 Cada elemento da matriz recebe valor val. */
-static int **MATRIXint( int r, int c, int val) { 
-   vertex i, j;
+static int **alocaMatriz( int r, int c, int val) { 
+   vertice i, j;
    int **m = malloc( r * sizeof (int *));
    for (i = 1; i <= r; ++i) 
       m[i] = malloc( c * sizeof (int));
@@ -58,38 +58,38 @@ static int **MATRIXint( int r, int c, int val) {
    return m;
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHinit() constrói um grafo com vértices 1,2 .. V e nenhum arco. */
-Graph GRAPHinit( int V) { 
-   Graph G = malloc( sizeof *G);
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função inicializaGrafo() constrói um grafo com vértices 1,2 .. V e nenhum arco. */
+Grafo inicializaGrafo( int V) { 
+   Grafo G = malloc( sizeof *G);
    G->V = V; 
    G->A = 0;
-   G->adj = MATRIXint( V, V, INF);
+   G->adj = alocaMatriz( V, V, INF);
    return G;
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHinsertArc() insere um arco v-w com custo c no grafo G.
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função insereArco() insere um arco v-w com custo c no grafo G.
 A função supõe que v e w são distintos, positivos e menores que G->V. Se o grafo já tem um arco v-w, a função não faz nada. */
 
-void GRAPHinsertArc( Graph G, vertex v, vertex w, int c) { 
+void insereArco( Grafo G, vertice v, vertice w, int c) { 
    if (G->adj[v][w] == INF) {
       G->adj[v][w] = c; 
       G->A++;
    }
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHremoveArc() remove do grafo G o arco v-w.
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função removeArco() remove do grafo G o arco v-w.
 A função supõe que v e w são distintos, positivos e menores que G->V. Se não existe arco v-w, a função não faz nada. */
-void GRAPHremoveArc( Graph G, vertex v, vertex w) { 
+void removeArco( Grafo G, vertice v, vertice w) { 
    if (G->adj[v][w] != INF) {
       G->adj[v][w] = -1; 
       G->A--;
    }
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função GRAPHshow() imprime, para cada vértice v do grafo G,
+/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função mostraGrafo() imprime, para cada vértice v do grafo G,
 em uma linha, todos os vértices adjacentes a v, além dos custos de cada arco. */
-void GRAPHshow( Graph G) { 
-   vertex v,w;
+void mostraGrafo( Grafo G) { 
+   vertice v,w;
    for (v = 1; v <= G->V; ++v) {
       printf( "%2d:", v);
       for (w = 1; w <= G->V; ++w)
@@ -109,38 +109,36 @@ void GRAPHshow( Graph G) {
    }
 }
 
-int minDistance(Graph G, int dist[], bool Z[]) 
+int distanciaMinima(Grafo G, int dist[], bool Z[]) 
 { 
-   // Initialize min value 
-   int min = INF, min_index; 
+   int minimo = INF, indiceMenorValor; 
    int v;
    for (v = 1; v <= G->V; v++) 
-     if (Z[v] == false && dist[v] <= min) 
-         min = dist[v], min_index = v; 
+     if (Z[v] == false && dist[v] <= minimo) 
+         minimo = dist[v], indiceMenorValor = v; 
    
-   return min_index; 
+   return indiceMenorValor; 
 } 
    
    
-void printPath(int anterior[], int j) 
+void mostraCaminho(int anterior[], int j) 
 { 
     if (anterior[j] == - 1){
     	printf("%d ", j);  
         return;
 	}
-    printPath(anterior, anterior[j]); 
+    mostraCaminho(anterior, anterior[j]); 
     
     printf("%d ", j); 
 } 
    
-// A utility function to print the constructed distance array 
-int printSolution(int dist[], int n, int anterior[], int s, int t) 
+int mostraSolucao(int dist[], int n, int anterior[], int s, int t) 
 { 
-    printPath(anterior, t); 
+    mostraCaminho(anterior, t); 
 } 
 
 
-int printDistances(int dist[], bool Z[], int anterior[], int n) 
+int mostraDistancias(int dist[], bool Z[], int anterior[], int n) 
 { 
 	int i;
    	printf("\nDistâncias\n"); 
@@ -159,53 +157,44 @@ int printDistances(int dist[], bool Z[], int anterior[], int n)
 	printf("\n");
 } 
    
-// Function that implements Dijkstra's single source shortest path algorithm 
-// for a graph represented using adjacency matrix representation 
-void Dijkstra(Graph G, int s, int t) 
+ 
+void Dijkstra(Grafo G, int s, int t) 
 { 
 	int dist[(G->V)+1];     
 	bool Z[(G->V)+1]; 
 	int anterior[(G->V)+1]; 
 	
 	int i, j, v;
-	// Initialize all distances as INFINITE and stpSet[] as false 
+	
 	for (i = 1; i <= G->V; i++){
 		anterior[i] = -1; 
 		dist[i] = INF, Z[i] = false; 
 	}
 	
-	// Distance of source vertex from itself is always 0 
 	dist[s] = 0; 
 	
-	// Find shortest path for all vertices 
 	for (j = 1; j <= G->V; j++) { 
 		
-	// Pick the minimum distance vertex from the set of vertices not 
-	// yet processed. u is always equal to src in the first iteration. 
-		int u = minDistance(G, dist, Z); 
-		printf("\nNo início da iteração sobre o vértice %d:", u);
-		printDistances(dist, Z, anterior,G->V);
-		// Mark the picked vertex as processed 
+		int u = distanciaMinima(G, dist, Z); 
+//		printf("\nNo início da iteração sobre o vértice %d:", u);
+//		mostraDistancias(dist, Z, anterior,G->V);
 		Z[u] = true; 
 		
-		// Update dist value of the adjacent vertices of the picked vertex. 
 		for (v = 1; v <= G->V; v++){
-		 // Update dist[v] only if is not in sptSet, there is an edge from  
-		 // u to v, and total weight of path from src to  v through u is  
-		 // smaller than current value of dist[v] 
+ 
 			if (!Z[v] && G->adj[u][v] != INF && dist[u] != INF && dist[u]+G->adj[u][v] < dist[v]){
 				anterior[v] = u; 
 		    	dist[v] = dist[u] + G->adj[u][v]; 
 			}
 		}
 		
-		printf("\nAo final da iteração:");
-		printDistances(dist, Z, anterior, G->V);
+//		printf("\nAo final da iteração:");
+//		mostraDistancias(dist, Z, anterior, G->V);
 	} 
 	
 	
 	printf("O custo mínimo de %d a %d é: %d\n", s, t, dist[t]);
-	printSolution(dist, G->V, anterior, s ,t); 
+	mostraSolucao(dist, G->V, anterior, s ,t); 
 } 
 
 void leituraArquivo(){
@@ -224,14 +213,13 @@ void leituraArquivo(){
 
     /* Variáveis para leitura e manipulação dos dados contidos no arquivo */
     int n, m, s, t;
-    vertex v, w;
+    vertice v, w;
     int c;
-    Graph G;
-    int coeficiente=0;
+    Grafo G;
 
     /* Variáveis auxiliares */
     int i=1, j=1;
-    int aux=0; //Variável auxiliar, para ler a primeira linha do arquivo (dimensao do sistema)
+    int aux=0; //Variável auxiliar, para ler a primeira linha do arquivo
     
     nomeArquivo = malloc(sizeof(char)*35); /* Aloca string com até 35 caracteres (valor arbitrário) */
 
@@ -264,7 +252,7 @@ void leituraArquivo(){
         while(!feof(file)){ /* Lê os dados do arquivo enquanto não chega no final do mesmo */
             if(aux==0){
                 fscanf(file,"%d %d %d %d",&n, &m, &s, &t); //Lê a primeira linha do arquivo, contendo n, m, s e t.
-                G = GRAPHinit(n);
+                G = inicializaGrafo(n);
 
                 printf("Numero de vertices: %d\n",n);
                 aux++; //Atualiza 'aux', para que este laço não seja mais executado nas próximas iterações.
@@ -272,13 +260,13 @@ void leituraArquivo(){
             } else{
                 /* Lê uma aresta do grafo */
                 fscanf(file,"%d %d %d",&v, &w, &c);
-                GRAPHinsertArc(G, v, w, c);
+                insereArco(G, v, w, c);
                 //v = 0; w = 0; c = 0;  //Reinicialização da variável, para receber novo valor na próxima iteração.
                 setbuf(stdin,NULL);
             }
         }
         
-        GRAPHshow(G);
+        mostraGrafo(G);
        
         Dijkstra(G, s, t); /*chama a função dijkstra passando o vértice origem, o que vai nos devolver o custo mínimo 
 						pra sair da origem e chegar em todos os outros vértices */
