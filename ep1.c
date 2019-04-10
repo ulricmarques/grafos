efcd4acbd26b43ad5a18763e43a5041fe022c0f2
Ulric Marques <ulricmf@gmail.com>
2019-04-10 11:31:51 -0300

    Adicionando alguns comentários e removendo funções desnecessárias

--- ep1.c
+++ ep1.c
@@ -17,104 +17,103 @@
 #include <locale.h>
 #include <limits.h>
 
-/* Vértices de grafos são representados por objetos do tipo vertice. */ 
+/* Vértices de grafos são representados por variáveis do tipo 'vertice',
+para facilitar a identificação.*/ 
 #define vertice int
+
+/* Variáveis do tipo 'INF' representam o valor infinito, que será usado para indicar
+que dois vértices não possuem ligação. Também será usado durante o algoritmo de Dijkstra,
+para indicar que não é possível chegar a um vértice partindo da origem. */
 #define INF INT_MAX
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A estrutura grafo representa um grafo. 
-	O campo adj é um ponteiro para a matriz de adjacências do grafo. 
-	O campo V contém o número de vértices e o campo A contém o número de arcos do grafo. */
+/* A estrutura 'grafo' representa um grafo. 
+	 O campo 'adj' é um ponteiro para a matriz de adjacências do grafo. 
+	 O campo 'V' contém o número de vértices e o campo 'A' contém o número de arcos do grafo. */
 	struct grafo {
 	   int V; 
 	   int A; 
 	   int **adj; 
 	};
 	
-	/* Um Grafo é um ponteiro para um grafo, ou seja, um Grafo contém o endereço de um grafo. */
+	/* Um 'Grafo' é um ponteiro para um grafo, ou seja, um 'Grafo' contém o endereço de um grafo. */
 	typedef struct grafo *Grafo;
 
-void limpaTela(){
-    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
-        system("clear");
-    #endif
 
-    #if defined(_WIN32) || defined(_WIN64)
-        system("cls");
-    #endif
+
+void limpaTela(){
+	#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+		system("clear");
+	#endif
+
+	#if defined(_WIN32) || defined(_WIN64)
+		system("cls");
+	#endif
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função alocaMatriz() aloca uma matriz com linhas 1..l e colunas 1..c. 
+/* A função alocaMatriz() aloca uma matriz com linhas 1,2,...,l e colunas 1..c. 
 Cada elemento da matriz recebe valor val. */
-static int **alocaMatriz( int l, int c, int val) { 
+static int **alocaMatriz(int l, int c, int val) { 
    vertice i, j;
-   int **m = malloc( (l+1) * sizeof (int *));
+   int **m = malloc((l+1) * sizeof (int *));
    for (i = 1; i <= l; ++i) 
-      m[i] = malloc( (c+1) * sizeof (int));
+	  m[i] = malloc((c+1) * sizeof (int));
    for (i = 1; i <= l; ++i)
-      for (j = 1; j <= c; ++j)
-         m[i][j] = val;
+	  for (j = 1; j <= c; ++j)
+		 m[i][j] = val;
    return m;
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função inicializaGrafo() constrói um grafo com vértices 1,2 .. V e nenhum arco. */
-Grafo inicializaGrafo( int V) { 
-   Grafo G = malloc( sizeof *G);
+/* A função inicializaGrafo() constrói um grafo com vértices 1,2,...,V e nenhum arco. */
+Grafo inicializaGrafo(int V) { 
+   Grafo G = malloc(sizeof *G);
    G->V = V; 
    G->A = 0;
-   G->adj = alocaMatriz( V, V, INF);
+   G->adj = alocaMatriz(V, V, INF);
    return G;
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função insereArco() insere um arco v-w com custo c no grafo G.
+/* A função insereArco() insere um arco v-w com custo c no grafo G.
 A função supõe que v e w são distintos, positivos e menores que G->V. Se o grafo já tem um arco v-w, a função não faz nada. */
-
-void insereArco( Grafo G, vertice v, vertice w, int c) { 
+void insereArco(Grafo G, vertice v, vertice w, int c) { 
    if (G->adj[v][w] == INF) {
-      G->adj[v][w] = c; 
-      G->A++;
+	  G->adj[v][w] = c; 
+	  G->A++;
    }
 }
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função removeArco() remove do grafo G o arco v-w.
-A função supõe que v e w são distintos, positivos e menores que G->V. Se não existe arco v-w, a função não faz nada. */
-void removeArco( Grafo G, vertice v, vertice w) { 
-   if (G->adj[v][w] != INF) {
-      G->adj[v][w] = -1; 
-      G->A--;
-   }
-}
 
-/* REPRESENTAÇÃO POR MATRIZ DE ADJACÊNCIAS: A função mostraGrafo() imprime, para cada vértice v do grafo G,
-em uma linha, todos os vértices adjacentes a v, além dos custos de cada arco. */
-void mostraGrafo( Grafo G) { 
-   vertice v,w;
-   printf( "\n");
-   for (v = 1; v <= G->V; ++v) {
-      printf( "%2d:", v);
-      for (w = 1; w <= G->V; ++w)
-         if (G->adj[v][w] != INF) 
-            printf( " %2d", w);
-      printf( "\n");
-   }
+/* A função mostraGrafo() imprime, para cada vértice v do grafo G,
+em uma linha, todos os vértices adjacentes a v. Também imprime os custos de cada arco. */
+void mostraGrafo(Grafo G) { 
+	vertice v,w;
+	printf("\n");
+	for (v = 1; v <= G->V; ++v) {
+		printf("%2d:", v);
+		for (w = 1; w <= G->V; ++w)
+			if (G->adj[v][w] != INF) 
+				printf(" %2d", w);
+		printf("\n");
+	}
    
-   printf( "\n");
+	printf("\n");
    
-   for (v = 1; v <= G->V; ++v) {
-      for (w = 1; w <= G->V; ++w)
-         if (G->adj[v][w] != INF){
-            printf("Arco: %2d->%2d Custo: %2d", v, w, G->adj[v][w]);
-			printf("\n");
+	for (v = 1; v <= G->V; ++v) {
+		for (w = 1; w <= G->V; ++w){
+			if (G->adj[v][w] != INF){
+				printf("Arco: %2d->%2d Custo: %2d\n", v, w, G->adj[v][w]);
+			}
 		}
    }
 }
+
 
 int distanciaMinima(Grafo G, int dist[], int Z[]) 
 { 
    int minimo = INF, indiceMenorValor; 
    int v;
    for (v = 1; v <= G->V; v++) 
-     if (Z[v] == 0 && dist[v] <= minimo) 
-         minimo = dist[v], indiceMenorValor = v; 
+	 if (Z[v] == 0 && dist[v] <= minimo) 
+		 minimo = dist[v], indiceMenorValor = v; 
    
    return indiceMenorValor; 
 } 
@@ -122,18 +121,19 @@ int distanciaMinima(Grafo G, int dist[], int Z[])
    
 void mostraCaminho(int anterior[], int t) 
 { 
-    if (anterior[t] == - 1){
-    	printf("\nCaminho: ");
-        return;
+	if (anterior[t] == - 1){
+		printf("\nCaminho: ");
+		return;
 	}
 	
-    mostraCaminho(anterior, anterior[t]); 
-    printf("(%d %d) ", anterior[t], t); 
+	mostraCaminho(anterior, anterior[t]); 
+	printf("(%d %d) ", anterior[t], t); 
 } 
    
 
 void Dijkstra(Grafo G, int s, int t) 
-{ 
+{
+
 	int dist[(G->V)+1];     
 	int Z[(G->V)+1]; 
 	int anterior[(G->V)+1]; 
@@ -156,154 +156,108 @@ void Dijkstra(Grafo G, int s, int t)
  
 			if (!Z[v] && G->adj[u][v] != INF && dist[u] != INF && dist[u]+G->adj[u][v] < dist[v]){
 				anterior[v] = u; 
-		    	dist[v] = dist[u] + G->adj[u][v]; 
+				dist[v] = dist[u] + G->adj[u][v]; 
 			}
 		}
 		
 	} 
 	
-	
+
 	printf("\nO custo mínimo do vértice %d ao vértice %d é: %d\n", s, t, dist[t]);
 	mostraCaminho(anterior, t); 
 	
 	
 	/* Espera o usuario pressionar uma tecla para voltar ao menu principal */
-    printf("\n\n[i] Pressione <ENTER> para voltar ao MENU DE OPCOES...");
-    setbuf(stdin,NULL);
-    getchar();
-    
+	printf("\n\n[i] Pressione <ENTER> para voltar ao MENU DE OPCOES...");
+	setbuf(stdin,NULL);
+	getchar();
+	
 
 } 
 
 void leituraArquivo(){
-    /**   Lê o nome do arquivo e, ao localizá-lo, lê o grafo contido no mesmo,
-     *    bem como os custos de seus arcos, e encontra um caminho de custo mínimo
-     	  utilizando o algoritmo de Dijkstra.
-     */
+	/**   Lê o nome do arquivo e, ao localizá-lo, lê o grafo contido no mesmo,
+	 *    bem como os custos de seus arcos, e encontra um caminho de custo mínimo
+		  utilizando o algoritmo de Dijkstra.
+	 */
 
-    /* Cabeçalho */
-    limpaTela();
-    printf("### Receber grafo a partir de um arquivo ###\n\n");
+	/* Cabeçalho */
+	limpaTela();
+	printf("### Receber grafo a partir de um arquivo ###\n\n");
 
-    /* Variáveis para manipulação do arquivo */
-    char *nomeArquivo;
-    FILE *file;
+	/* Variáveis para manipulação do arquivo */
+	char *nomeArquivo;
+	FILE *file;
 
-    /* Variáveis para leitura e manipulação dos dados contidos no arquivo */
-    int n, m, s, t;
-    vertice v, w;
-    int c;
-    Grafo G;
+	/* Variáveis para leitura e manipulação dos dados contidos no arquivo */
+	int vertices, arcos, origem, destino;
+	vertice v, w;
+	int custo;
+	Grafo G;
 
-    /* Variáveis auxiliares */
-    int i=0;
-    
-    nomeArquivo = malloc(sizeof(char)*35); /* Aloca string com até 35 caracteres (valor arbitrário) */
+	/* Variáveis auxiliares */
+	int i=0;
+	
+	nomeArquivo = malloc(sizeof(char)*35); /* Aloca string com até 35 caracteres (valor arbitrário) */
 
-    if(nomeArquivo == NULL){
-        printf("[x] Falha! Nao ha memoria disponivel no sistema");
-        free(nomeArquivo);
-        return;
-    }
-    setbuf(stdin,NULL);
-    printf("Digite o nome do arquivo de texto (SEM SUA EXTENSAO): ");
-    scanf(" %s", nomeArquivo);   //Lê o nome do arquivo;
+	if(nomeArquivo == NULL){
+		printf("[x] Falha! Não há memória disponível no sistema");
+		free(nomeArquivo);
+		return;
+	}
+	setbuf(stdin,NULL);
+	printf("Digite o nome do arquivo de texto (SEM SUA EXTENSÃO): ");
+	scanf(" %s", nomeArquivo);   //Lê o nome do arquivo;
 
-    strcat(nomeArquivo,".txt\0"); //Adiciona a extensão '.txt' mais caractere '\0' (final da string) ao nome do arquivo;
+	strcat(nomeArquivo,".txt\0"); //Adiciona a extensão '.txt' mais caractere '\0' (final da string) ao nome do arquivo;
 
-    file = fopen(nomeArquivo,"r"); //Abre arquivo no modo de leitura;
+	file = fopen(nomeArquivo,"r"); //Abre arquivo no modo de leitura;
 
-    if(file == NULL){ /* Arquivo não encontrado */
-        free(nomeArquivo);
-        printf("\n\n[!] Arquivo nao localizado");
-        printf("\n[i] Verifique se o arquivo existe ou se voce digitou o nome corretamente");
-        printf("\n\n[i] Pressione <ENTER> para voltar ao MENU DE OPCOES...");
-        setbuf(stdin,NULL);
-        getchar();
+	if(file == NULL){ /* Arquivo não encontrado */
+		free(nomeArquivo);
+		printf("\n\n[!] Arquivo não localizado");
+		printf("\n[i] Verifique se o arquivo existe ou se você digitou o nome corretamente");
+		printf("\n\n[i] Pressione <ENTER> para voltar ao MENU DE OPCÕES...");
+		setbuf(stdin,NULL);
+		getchar();
 
-        return;
-    }
-    else{
-        printf("\n\tArquivo: %s\t",nomeArquivo);
+		return;
+	}
+	else{
+		printf("\n\tArquivo: %s\t",nomeArquivo);
 
-		fscanf(file,"%d %d %d %d",&n, &m, &s, &t); //Lê a primeira linha do arquivo, contendo n, m, s e t.
-		printf("\nVértices: %d\tArcos: %d\t Origem: %d\t Destino: %d\n", n, m, s, t);
-    	G = inicializaGrafo(n);
-        while(i < m){ /* Lê as próximas m linhas contendo os arcos do grafo */
-            /* Lê um arco do grafo */
-            fscanf(file,"%d %d %d",&v, &w, &c);
-            insereArco(G, v, w, c);
-            i++;
-            setbuf(stdin,NULL);
-        }
-        
-        mostraGrafo(G);
-       
-        Dijkstra(G, s, t); /*chama a função Dijkstra passando o vértice s(origem) e o vértice t(destino), 
-						o que vai nos devolver o custo mínimo para sair de s e chegar em t. */
-        fclose(file); //Fecha o arquivo
+
+	/*Lê a primeira linha do arquivo, contendo a quantidade de vértices,
+	a quantidade de arcos, o vértice origem e o vértice destino. */
+		fscanf(file,"%d %d %d %d",&vertices, &arcos, &origem, &destino); 
+
+		printf("\nVértices: %d\tArcos: %d\t Origem: %d\t Destino: %d\n", vertices, arcos, origem, destino);
+		G = inicializaGrafo(vertices);
+		while(i < arcos){ /* Lê as próximas m linhas contendo os arcos do grafo */
+			/* Lê um arco do grafo */
+			fscanf(file,"%d %d %d",&v, &w, &custo);
+			insereArco(G, v, w, custo);
+			i++;
+			setbuf(stdin,NULL);
+		}
+		
+		mostraGrafo(G);
+	   
+		/*chama a função Dijkstra passando o vértice origem e o vértice destino, 
+			o que vai nos devolver o custo mínimo para sair da origem e chegar ao destino. */
+		Dijkstra(G, origem, destino); 
+
+		fclose(file); //Fecha o arquivo
 	}
 }
-
- char menuDeOpcoes(){
-    /** Exibe o menu de opções, lê a opção escolhida pelo usuário e retorna essa opção */
-
-    char opcao;
-
-    //Mostra o menu de opções. Repete o processo enquanto a opção escolhida pelo usuário não for válida.
-    do{
-        //Monta menu.
-        limpaTela();
-        printf("### GRAFOS ###\n\n");
-        printf("### MENU DE OPCOES ###\n\n");
-        printf("C - Caminho minimo\n");
-        printf("F - Finalizar\n");
-        printf("\n[i] Escolha uma opcao acima e pressione <ENTER>: ");
-
-        setbuf(stdin,NULL);
-        opcao = toupper(getchar()); //Lê a opção digitada pelo usuário e a transforma em letra maiúscula.
-
-        /* Exibe aviso de advertência, caso a opção escolhida pelo usuário seja inválida */
-        if((opcao!='C') && (opcao!='F')){
-            printf("#");
-            printf("\n\n[!] Opcao invalida\n");
-            printf("[i] Pressione <ENTER> e tente novamente...");
-            setbuf(stdin,NULL);
-            getchar();
-        }
-    //Testa validade. Se a opção escolhida for válida, o laço termina. Caso contrário, mostra o menu novamente.
-    } while((opcao!='C') && (opcao!='F'));
-
-    return opcao;
-} /* Fim menuDeOpcoes() */
-
- /* ===================== FIM SEÇÃO 04 ===================== */
 
 
 /** FUNÇÃO PRINCIPAL */
 int main(){
-    setlocale(LC_ALL, "portuguese");
-    int finalizarPrograma = 0; //Flag para quando o usuário desejar finalizar o programa.
-    
+	setlocale(LC_ALL, "portuguese");
+	int finalizarPrograma = 0; //Flag para quando o usuário desejar finalizar o programa.
+	
 
-    /* Laço do-while: Chama a função 'menuDeOpções' e abre a função correspondente à escolha do usuário.
-     * Repete o processo enquanto o mesmo não desejar finalizar o programa.
-     */
-    do{
-        switch(menuDeOpcoes()){
-            //Opção 'CAMINHO MINIMO'
-            case 'C':
-                leituraArquivo();
-                break;
-
-            //Opção 'FINALIZAR'
-            case 'F':
-                printf("Finalizar\n");
-                finalizarPrograma = 1;
-                break;
-        }
-    //Testa validade para saber se o usuário deseja finalizar o programa.
-    } while(!finalizarPrograma);
-
+	leituraArquivo();
 	return 0;
 } /* Fim main() */
